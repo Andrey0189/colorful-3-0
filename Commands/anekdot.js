e17aa6fb7116d595bcd55f2fa956ef4889bfff0f
Andrew Varnavsky <39239738+Andrey0189@users.noreply.github.com>
2021-11-03 19:55:49 +0300

    Add files via upload

---
+++ Commands/anekdot.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'anekdot',
+    regex: /anekdot/,
+    desc: 'Анекдоты (не работают)',
+    run: (message) => {
+    }
+}
--- Commands/ping.js
+++ Commands/ping.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+    name: 'ping',
+    regex: /ping|пин[кг]/,
+    desc: 'Пинг',
+    run: (message) => {
+    const embed = new Bot.Discord.MessageEmbed()
+        .setAuthor('Pong!', message.author.avatarURL)
+        .setDescription(`**Discrod API - \`${Math.round(Bot.client.ws.ping)} ms\`**`)
+        .setFooter(`${Bot.name} ${Bot.version}`)
+        .setColor(Bot.colors.blurple);
+        message.channel.send(embed);
+    }
+}
