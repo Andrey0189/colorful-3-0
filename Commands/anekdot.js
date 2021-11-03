71ee0d7eda860f16602ce1cc021e25aca29717d4
a73435 <h2drhxj4@anonaddy.me>
2021-11-03 23:39:10 +0300

    Анекдот 2

--- Commands/anekdot.js
+++ Commands/anekdot.js
@@ -8,7 +8,7 @@ module.exports = {
             const res = await fetch("https://www.anekdot.ru/rss/randomu.html", { headers: { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0" } });
             const anekText = await res.text();
             if(res.ok) {
-                const anek = anekText.slice(138).split('\\",\\"')[0].replaceAll("<br>", "\n").replace('"', '"');
+                const anek = anekText.slice(138).split('\\",\\"')[0].replace(/<br>/gm, "\n").replace(/\\\"/gm, '"');
                 const embed = new Bot.Discord.MessageEmbed()
                     .setTitle("Анекдот")
                     .setDescription(anek.slice(0, 4096))
