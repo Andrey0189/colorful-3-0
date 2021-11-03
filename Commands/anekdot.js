69522d6b3a1f1d6ee51d97387b62d4b00aac8caf
a73435 <h2drhxj4@anonaddy.me>
2021-11-03 23:28:20 +0300

    Анекдоты

--- Commands/anekdot.js
+++ Commands/anekdot.js
@@ -1,7 +1,23 @@
 module.exports = {
     name: 'anekdot',
-    regex: /anekdot/,
-    desc: 'Анекдоты (не работают)',
-    run: (message) => {
+    regex: /a(nek)?d(ot)?/,
+    desc: 'Случайный анекдот',
+    run: (message, args) => {
+        const fetch = require("node-fetch");
+        async function anekdot() {
+            const res = await fetch("https://www.anekdot.ru/rss/randomu.html", { headers: { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0" } });
+            const anekText = await res.text();
+            if(res.ok) {
+                const anek = anekText.slice(138).split('\\",\\"')[0].replaceAll("<br>", "\n").replace('"', '"');
+                const embed = new Bot.Discord.MessageEmbed()
+                    .setTitle("Анекдот")
+                    .setDescription(anek.slice(0, 4096))
+                    .setColor(Bot.colors.blurple)
+                message.channel.send(embed)
+            } else {
+                message.channel.send("Ошибка API")
+            }
+        }
+        anekdot()
     }
 }
