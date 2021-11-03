ed1d1d2ad665fb0423942289d2bcfea68c626a89
Andrew Varnavsky <39239738+Andrey0189@users.noreply.github.com>
2021-11-03 19:59:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ class Bot {
         this.Discord = Discord;
         this.fs = fs;
 
-        this.client = new Discord.Client({disableEveryone: true});
+        this.client = new Discord.Client({disableMentions: 'everyone'});
         this.client.login('NDcyMDQ4MzgzMDc1NTQ5MTg2.W1nbUA.NK4aC5OOBXCWD7g2cDfivk4iNag').then(() => delete process.env.BOT_TOKEN);
 
         this.name = 'Colorful';
