76a3b84678430b2724593fcb13119982efd305f4
dasehak <dasehak3@gmail.com>
2021-11-04 15:23:05 +0300

    Порфирьевич

---
+++ Commands/porfirevich.js
@@ -0,0 +1,29 @@
+module.exports = {
+    name: 'gentext',
+    regex: /gen(text)?/,
+    desc: 'Генерация текста с помощью Порфирьевича',
+    run: async (message, args) => {
+        const fetch = require("node-fetch");
+        const choose = (arr) => {
+            let index = Math.floor(Math.random() * arr.length);
+            return arr[index];
+        }
+
+        const body = {prompt: args.join(" "), length: 30};
+        const res = await fetch("https://pelevin.gpt.dobro.ai/generate/", {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {'Content-Type': 'application/json', "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0"}
+        });
+        const gen = await res.json();
+        if(!gen.error && res.ok) {
+            const embed = new Bot.Discord.MessageEmbed()
+                .setTitle("Генерация Текста")
+                .addField(args.join(" "), args.join(" ") + choose(gen['replies']), true)
+                .setColor(Bot.colors.blurple)
+            await message.channel.send(embed)
+        } else {
+            await message.channel.send("Ошибка API")
+        }
+    }
+}
