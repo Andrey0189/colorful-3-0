6d6d966dc32b52d10fe44517affdb396c70f6ca2
Andrey0189 <varnavsky06@gmail.com>
2021-11-04 12:42:16 +0300

    simpler ping

--- Commands/ping.js
+++ Commands/ping.js
@@ -3,11 +3,6 @@ module.exports = {
     regex: /ping|пин[кг]/,
     desc: 'Пинг',
     run: async (message) => {
-    const embed = new Bot.Discord.MessageEmbed()
-        .setAuthor('Pong!', message.author.avatarURL)
-        .setDescription(`**Discrod API - \`${Math.round(Bot.client.ws.ping)} ms\`**`)
-        .setFooter(`${Bot.name} ${Bot.version}`)
-        .setColor(Bot.colors.blurple);
-        await message.channel.send(embed);
+        await message.channel.send(`**\`${Math.round(Bot.client.ws.ping)}\` ms**`);
     }
 }
