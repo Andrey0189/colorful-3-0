b940ec8ae4b83351608f0e167c5d302716c8085a
Andrey0189 <varnavsky06@gmail.com>
2021-11-08 00:56:20 +0300

    sys

--- Commands/eval.js
+++ Commands/eval.js
@@ -4,9 +4,9 @@ module.exports = {
     run: async (message, args) => {
       try {
 
-        const output = eval(`const verySussyFunction = async () => { return ${args.join(' ')} }; verySussyFunction();`);
+        //const output = eval(`const verySussyFunction = async () => { return ${args.join(' ')} }; verySussyFunction();`);
 
-        await message.channel.send(`//Success ✅\n${output}`, {code: 'js', split: '\n'});
+        await message.channel.send(`//Success ✅\n${eval(args.join(' '))}`, {code: 'js', split: '\n'});
       } catch (err) {
         await message.channel.send(`//Error ❎\n${err}`, {code: 'js'});
       }
