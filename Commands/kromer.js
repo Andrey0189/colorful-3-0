e56816031ef71253ba31b42e65ca6560527a1e93
Andrey0189 <varnavsky06@gmail.com>
2021-11-14 22:25:13 +0300

    [[BIG SHOT]]

---
+++ Commands/kromer.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'kromer',
+    regex: /kromer/,
+    hidden: true,
+    run: async (message) => {
+        const dir = 'Images/Kromer/';
+        const filesLength = Bot.fs.readdirSync(dir).length;
+        await message.channel.send({
+          files: [`${dir}${Bot.random(1, filesLength)}.png`]
+        });
+    };
+}
